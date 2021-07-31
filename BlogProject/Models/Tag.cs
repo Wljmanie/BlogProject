6dba0793c211f3f95e026039c27a386465dcba85
Wljmanie <45335409+Wljmanie@users.noreply.github.com>
2021-07-31 18:28:40 +0200

    Set up MS SQL Connection

--- BlogProject/Models/Tag.cs
+++ BlogProject/Models/Tag.cs
@@ -11,7 +11,7 @@ namespace BlogProject.Models
     {
         public int Id { get; set; }
         public int PostId { get; set; }
-        public string AuthorId { get; set; }
+        public string BlogUserId { get; set; }
 
         [Required]
         [StringLength(25, ErrorMessage = "The {0} must be at least {2} and at most {1} characters.", MinimumLength = 2)]
@@ -19,7 +19,7 @@ namespace BlogProject.Models
 
         //Navigation properties
         public virtual Post Post { get; set; }
-        public virtual BlogUser Author { get; set; }
+        public virtual BlogUser BlogUser { get; set; }
         
     }
 }
