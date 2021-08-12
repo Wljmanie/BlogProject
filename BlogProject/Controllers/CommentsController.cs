d58c0cdb3d447dbc1b0719612e5d156a3b61b7fa
Wljmanie <45335409+Wljmanie@users.noreply.github.com>
2021-08-12 10:20:32 +0200

    Editting Comments

--- BlogProject/Controllers/CommentsController.cs
+++ BlogProject/Controllers/CommentsController.cs
@@ -67,7 +67,7 @@ namespace BlogProject.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("PostId,Body")] Comment comment)
+        public async Task<IActionResult> Create([Bind("PostId,Body")] Comment comment, string Slug)
         {
             if (ModelState.IsValid)
             {
@@ -81,7 +81,7 @@ namespace BlogProject.Controllers
 
                 _context.Add(comment);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index)); //should return to the post detail action
+                return RedirectToAction("Details", "Posts", new { slug = Slug }, "commentSection"); //should return to the post detail action
             }
             //ViewData["BlogUserId"] = new SelectList(_context.Users, "Id", "Id", comment.BlogUserId);
             //ViewData["ModeratorId"] = new SelectList(_context.Users, "Id", "Id", comment.ModeratorId);
@@ -115,7 +115,7 @@ namespace BlogProject.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,PostId,BlogUserId,ModeratorId,Body,Created,Updated,Moderated,Deleted,ModeratedBody,ModerationType")] Comment comment)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Body")] Comment comment)
         {
             if (id != comment.Id)
             {
@@ -123,10 +123,16 @@ namespace BlogProject.Controllers
             }
 
             if (ModelState.IsValid)
-            {
+            { 
+                var newComment = await _context.Comments.Include(c => c.Post).FirstOrDefaultAsync(c => c.Id == comment.Id);
                 try
                 {
-                    _context.Update(comment);
+                   
+
+                    newComment.Body = comment.Body;
+                    newComment.Updated = DateTime.Now;
+
+                    //_context.Update(comment);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
@@ -140,11 +146,9 @@ namespace BlogProject.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Details", "Posts", new { slug = newComment.Post.Slug },"commentSection");
             }
-            ViewData["BlogUserId"] = new SelectList(_context.Users, "Id", "Id", comment.BlogUserId);
-            ViewData["ModeratorId"] = new SelectList(_context.Users, "Id", "Id", comment.ModeratorId);
-            ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Abstract", comment.PostId);
+            
             return View(comment);
         }
 
